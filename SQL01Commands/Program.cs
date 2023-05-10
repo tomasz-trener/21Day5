d4f203b9143d8aa9ef682b9cae07506522594f41
Tomasz Leś <szkolenia@tomaszles.pl>
2023-05-10 11:57:13 +0200

    Add project files.

---
+++ SQL01Commands/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Data.SqlClient;
+
+string connectionString =
+    "Server=(localdb)\\mssqllocaldb;Database=VolleyballDatabase;Integrated Security=True;";
+
+using (SqlConnection connection = new SqlConnection(connectionString))
+{
+
+    connection.Open();
+    //using (SqlCommand command =
+    //    new SqlCommand("INSERT INTO Persons (FirstName, LastName) VALUES(@FirstName, @LastName)", connection))
+    //{
+    //    command.Parameters.AddWithValue("@FirstName", "John");
+    //    command.Parameters.AddWithValue("@LastName", "Doe");
+    //    int rowsAffected = command.ExecuteNonQuery();
+    //    Console.WriteLine($"{rowsAffected} rows affected.");
+    //}
+
+
+    using (SqlCommand command =
+        new SqlCommand("SELECT Id, FirstName, LastName FROM Persons", connection))
+    {
+        using (SqlDataReader reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                Console.WriteLine($"" +
+                    $"Id: {reader.GetInt32(0)}, " +
+                    $"FirstName: {reader.GetString(1)}, " +
+                    $"LastName: {reader.GetString(2)}");
+            }
+        }
+    }
+
+    //using (SqlCommand command = 
+    //    new SqlCommand("UPDATE Persons SET FirstName =@FirstName WHERE Id = @Id", connection))
+    //{
+    //command.Parameters.AddWithValue("@Id", 6);
+    //    command.Parameters.AddWithValue("@FirstName", "UpdatedFirstName");
+    //    int rowsAffected = command.ExecuteNonQuery();
+    //    Console.WriteLine($"{rowsAffected} rows affected.");
+    //}
+
+    try
+    {
+        //int a = 0;
+        //int b = 5 / a;
+        using (SqlCommand command = new SqlCommand("DELETE FROM Person WHERE Id = @Id", connection))
+        {
+            command.Parameters.AddWithValue("@Id", 6);
+            int rowsAffected = command.ExecuteNonQuery();
+            Console.WriteLine($"{rowsAffected} rows affected.");
+        }
+    }
+    catch (SqlException ex)
+    {
+        Console.WriteLine("An error occurred while connecting to the database or executing the command: " + ex.Message);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("An unexpected error occurred: " + ex.Message);
+    }
+
+
+
+
+}
+
+
+
---
+++ SQL02Repositories/PeronsRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL02Repositories
+{
+    internal class PeronsRepository
+    {
+       private string connectionString =
+            "Server=(localdb)\\mssqllocaldb;Database=VolleyballDatabase;Integrated Security=True;";
+
+        public Person[] GetPersons()
+        {
+            List<Person> persons = new List<Person>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                connection.Open();
+                using (SqlCommand command =
+                    new SqlCommand("SELECT Id, FirstName, LastName FROM Persons", connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Person person = new Person();
+                            person.Id = reader.GetInt32(0);
+                            person.FirstName = reader.GetString(1);
+                            person.LastName = reader.GetString(2);
+                            persons.Add(person);
+                        }
+                    }
+                }
+            }
+            return persons.ToArray();
+
+        }
+    }
+}
